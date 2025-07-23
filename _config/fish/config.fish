2948af38e3f5076acd0f9513701e87fa545dbce7
Dchavez430 <61951399+Dchavez430@users.noreply.github.com>
2025-07-23 14:42:30 -0600

    Delete _config/fish directory

--- _config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-set PATH $PATH:/opt/Xilinx/SDK/2019.1/bin:/opt/Xilinx/Vivado/2019.1/bin
-export EDITOR=nvim
-alias vim="nvim"
-alias vi="nvim"
-alias vimdiff='nvim -d' 
-fish_vi_key_bindings
-end
